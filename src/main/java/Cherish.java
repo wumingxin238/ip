fb04582f83aea2b3a7464f6db2d57c984f565408
wumingxin238 <1208399599@qq.com>
2026-01-22 23:44:49 +0800

    level 0

---
+++ src/main/java/Cherish.java
@@ -0,0 +1,14 @@
+public class Cherish {
+    public static void main(String[] args) {
+
+    }
+
+    public void greet() {
+        System.out.println("Hello! I'm Cherish\n" +
+                "What can I do for you?\n");
+    }
+
+    public void exit() {
+        System.out.println(" Bye. Hope to see you again soon!\n");
+    }
+}
