035de6d21902b219c876d38fd38cd24733794a8b
wumingxin238 <1208399599@qq.com>
2026-01-22 23:56:10 +0800

    level 2

--- src/main/java/Cherish.java
+++ src/main/java/Cherish.java
@@ -1,6 +1,27 @@
-public class Cherish {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Cherish {
+
+    private static String[] tasks = new String[100];
+    private static int taskCount = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Cherish ch = new Cherish();
+        ch.greet();
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                printTaskList();
+            } else {
+                addTask(input);
+            }
+        }
+        ch.exit();
+        sc.close();
     }
 
     public void greet() {
@@ -10,5 +31,17 @@ public class Cherish {
 
     public void exit() {
         System.out.println(" Bye. Hope to see you again soon!\n");
+    }
+
+    private static void addTask(String task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        System.out.println("added: " + task);
+    }
+
+    private static void printTaskList() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
     }
 }
